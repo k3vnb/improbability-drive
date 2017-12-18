dc300cd52e6228655ab3e859a58fb01e06cfabee
Zachary Marston <zmarston@live.com>
2017-12-18 14:58:46 -0800

    Add planet mixin and .gitignore

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
   $(".planet-1").click(function() {
     $('.planet-1').toggleClass('transform-active');
+    $('.explore').toggleClass('explore-show');
   });
-  $(".button").click(function() {
+  $(".explore").click(function() {
     $('.planet-1').toggleClass('explore-planet')
     $('.alien-1').toggleClass('alien-grow')
+    $('.return-to-orbit').toggleClass('return-to-orbit-show')
   })
+  $(".return-to-orbit").click(function() {
+    $('.planet-1').toggleClass('explore-planet')
+    $('.alien-1').toggleClass('alien-grow')
+    $('.alien-1').toggleClass('alien-shrink')
+    $('.alien-1').toggleClass('alien-shrink')
+    $('.return-to-orbit').toggleClass('return-to-orbit-show')
+  })
+
 });
