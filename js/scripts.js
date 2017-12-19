ca4a4960b1b06634b511b343c7ef4c6ad09ec90a
Zachary Marston <zmarston@live.com>
2017-12-19 13:22:42 -0800

    add functions to hide other planets

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+
+
   $(".planet-1").click(function() {
     $('.planet-1').toggleClass('transform-active');
     $('.explore-1').toggleClass('explore-show');
-    $('.explore-2, .explore-3, .explore-4, .explore-5').removeClass('explore-show');
-    $('.planet-2, .planet-3, .planet-4, .planet-5').removeClass('transform-active');
+    $('.planet-2, .planet-3, .planet-4, .planet-5').toggleClass('go-away');
   });
   $(".explore-1").click(function() {
     $('.planet-1').toggleClass('explore-planet-1')
@@ -20,14 +21,16 @@ $(document).ready(function(){
     $('.explore-1').toggleClass('explore-show');
     $('.planet-1').addClass('on-top');
     setTimeout(function() {
+      $('.planet-2, .planet-3, .planet-4, .planet-5').toggleClass('go-away');
       $('.planet-1').removeClass('on-top');
     }, 3000);
   })
+
+
   $(".planet-2").click(function() {
     $('.planet-2').toggleClass('transform-active');
     $('.explore-2').toggleClass('explore-show');
-    $('.explore-1, .explore-3, .explore-4, .explore-5').removeClass('explore-show');
-    $('.planet-1, .planet-3, .planet-4, .planet-5').removeClass('transform-active');
+    $('.planet-5, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-2").click(function() {
     $('.planet-2').toggleClass('explore-planet-2')
@@ -44,14 +47,16 @@ $(document).ready(function(){
     $('.explore-2').toggleClass('explore-show');
     $('.planet-2').addClass('on-top');
     setTimeout(function() {
+      $('.planet-5, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-2').removeClass('on-top');
     }, 3000);
   })
+
+
   $(".planet-3").click(function() {
     $('.planet-3').toggleClass('transform-active');
     $('.explore-3').toggleClass('explore-show');
-    $('.explore-2, .explore-1, .explore-4, .explore-5').removeClass('explore-show');
-    $('.planet-2, .planet-1, .planet-4, .planet-5').removeClass('transform-active');
+    $('.planet-2, .planet-5, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-3").click(function() {
     $('.planet-3').toggleClass('explore-planet-3')
@@ -68,14 +73,16 @@ $(document).ready(function(){
     $('.explore-3').toggleClass('explore-show');
     $('.planet-3').addClass('on-top');
     setTimeout(function() {
+      $('.planet-2, .planet-5, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-3').removeClass('on-top');
     }, 3000);
   })
+
+
   $(".planet-4").click(function() {
     $('.planet-4').toggleClass('transform-active');
     $('.explore-4').toggleClass('explore-show');
-    $('.explore-2, .explore-3, .explore-1, .explore-5').removeClass('explore-show');
-    $('.planet-2, .planet-3, .planet-1, .planet-5').removeClass('transform-active');
+    $('.planet-2, .planet-3, .planet-5, .planet-1').toggleClass('go-away');
   });
   $(".explore-4").click(function() {
     $('.planet-4').toggleClass('explore-planet-4')
@@ -92,14 +99,16 @@ $(document).ready(function(){
     $('.explore-4').toggleClass('explore-show');
     $('.planet-4').addClass('on-top');
     setTimeout(function() {
+      $('.planet-2, .planet-3, .planet-5, .planet-1').toggleClass('go-away');
       $('.planet-4').removeClass('on-top');
     }, 3000);
   })
+
+
   $(".planet-5").click(function() {
     $('.planet-5').toggleClass('transform-active');
     $('.explore-5').toggleClass('explore-show');
-    $('.explore-2, .explore-3, .explore-4, .explore-1').removeClass('explore-show');
-    $('.planet-2, .planet-3, .planet-4, .planet-1').removeClass('transform-active');
+    $('.planet-2, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-5").click(function() {
     $('.planet-5').toggleClass('explore-planet-5')
@@ -116,6 +125,7 @@ $(document).ready(function(){
     $('.explore-5').toggleClass('explore-show');
     $('.planet-5').addClass('on-top');
     setTimeout(function() {
+      $('.planet-2, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-5').removeClass('on-top');
     }, 3000);
   })
