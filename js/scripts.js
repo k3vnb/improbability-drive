2e544011782a937026f9fc2aaadd04b3389c113a
Zachary Marston <zmarston@live.com>
2017-12-18 16:35:30 -0800

    add planets with functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,93 @@
 $(document).ready(function(){
   $(".planet-1").click(function() {
     $('.planet-1').toggleClass('transform-active');
-    $('.explore').toggleClass('explore-show');
+    $('.explore-1').toggleClass('explore-show');
   });
-  $(".explore").click(function() {
-    $('.planet-1').toggleClass('explore-planet')
+  $(".explore-1").click(function() {
+    $('.planet-1').toggleClass('explore-planet-1')
     $('.alien-1').toggleClass('alien-grow')
-    $('.return-to-orbit').toggleClass('return-to-orbit-show')
+    $('.return-to-orbit-1').toggleClass('return-to-orbit-show')
   })
-  $(".return-to-orbit").click(function() {
-    $('.planet-1').toggleClass('explore-planet')
+  $(".return-to-orbit-1").click(function() {
+    $('.planet-1').toggleClass('explore-planet-1')
     $('.alien-1').toggleClass('alien-grow')
     $('.alien-1').toggleClass('alien-shrink')
     $('.alien-1').toggleClass('alien-shrink')
-    $('.return-to-orbit').toggleClass('return-to-orbit-show')
+    $('.return-to-orbit-1').toggleClass('return-to-orbit-show')
+    $('.planet-1').toggleClass('transform-active');
+    $('.explore-1').toggleClass('explore-show');
+  })
+  $(".planet-2").click(function() {
+    $('.planet-2').toggleClass('transform-active');
+    $('.explore-2').toggleClass('explore-show');
+  });
+  $(".explore-2").click(function() {
+    $('.planet-2').toggleClass('explore-planet-2')
+    $('.alien-2').toggleClass('alien-grow')
+    $('.return-to-orbit-2').toggleClass('return-to-orbit-show')
+  })
+  $(".return-to-orbit-2").click(function() {
+    $('.planet-2').toggleClass('explore-planet-2')
+    $('.alien-2').toggleClass('alien-grow')
+    $('.alien-2').toggleClass('alien-shrink')
+    $('.alien-2').toggleClass('alien-shrink')
+    $('.return-to-orbit-2').toggleClass('return-to-orbit-show')
+    $('.planet-2').toggleClass('transform-active');
+    $('.explore-2').toggleClass('explore-show');
+  })
+  $(".planet-3").click(function() {
+    $('.planet-3').toggleClass('transform-active');
+    $('.explore-3').toggleClass('explore-show');
+  });
+  $(".explore-3").click(function() {
+    $('.planet-3').toggleClass('explore-planet-3')
+    $('.alien-3').toggleClass('alien-grow')
+    $('.return-to-orbit-3').toggleClass('return-to-orbit-show')
+  })
+  $(".return-to-orbit-3").click(function() {
+    $('.planet-3').toggleClass('explore-planet-3')
+    $('.alien-3').toggleClass('alien-grow')
+    $('.alien-3').toggleClass('alien-shrink')
+    $('.alien-3').toggleClass('alien-shrink')
+    $('.return-to-orbit-3').toggleClass('return-to-orbit-show')
+    $('.planet-3').toggleClass('transform-active');
+    $('.explore-3').toggleClass('explore-show');
+  })
+  $(".planet-4").click(function() {
+    $('.planet-4').toggleClass('transform-active');
+    $('.explore-4').toggleClass('explore-show');
+  });
+  $(".explore-4").click(function() {
+    $('.planet-4').toggleClass('explore-planet-4')
+    $('.alien-4').toggleClass('alien-grow')
+    $('.return-to-orbit-4').toggleClass('return-to-orbit-show')
+  })
+  $(".return-to-orbit-4").click(function() {
+    $('.planet-4').toggleClass('explore-planet-4')
+    $('.alien-4').toggleClass('alien-grow')
+    $('.alien-4').toggleClass('alien-shrink')
+    $('.alien-4').toggleClass('alien-shrink')
+    $('.return-to-orbit-4').toggleClass('return-to-orbit-show')
+    $('.planet-4').toggleClass('transform-active');
+    $('.explore-4').toggleClass('explore-show');
+  })
+  $(".planet-5").click(function() {
+    $('.planet-5').toggleClass('transform-active');
+    $('.explore-5').toggleClass('explore-show');
+  });
+  $(".explore-5").click(function() {
+    $('.planet-5').toggleClass('explore-planet-5')
+    $('.alien-5').toggleClass('alien-grow')
+    $('.return-to-orbit-5').toggleClass('return-to-orbit-show')
+  })
+  $(".return-to-orbit-5").click(function() {
+    $('.planet-5').toggleClass('explore-planet-5')
+    $('.alien-5').toggleClass('alien-grow')
+    $('.alien-5').toggleClass('alien-shrink')
+    $('.alien-5').toggleClass('alien-shrink')
+    $('.return-to-orbit-5').toggleClass('return-to-orbit-show')
+    $('.planet-5').toggleClass('transform-active');
+    $('.explore-5').toggleClass('explore-show');
   })
 
 });
