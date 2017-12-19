338e1072696deded12af2a5bdb768df9443a4496
Zachary Marston <zmarston@live.com>
2017-12-19 14:11:11 -0800

    change explore text

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   $(".planet-1").click(function() {
     $('.planet-1').toggleClass('transform-active');
     $('.explore-1').toggleClass('explore-show');
+    $('.explore-text-1').toggleClass('explore-text-show');
     $('.planet-2, .planet-3, .planet-4, .planet-5').toggleClass('go-away');
   });
   $(".explore-1").click(function() {
@@ -19,17 +20,19 @@ $(document).ready(function(){
     $('.return-to-orbit-1').toggleClass('return-to-orbit-show')
     $('.planet-1').toggleClass('transform-active');
     $('.explore-1').toggleClass('explore-show');
+    $('.explore-text-1').toggleClass('explore-text-show');
     $('.planet-1').addClass('on-top');
     setTimeout(function() {
       $('.planet-2, .planet-3, .planet-4, .planet-5').toggleClass('go-away');
       $('.planet-1').removeClass('on-top');
-    }, 3000);
+    }, 2000);
   })
 
 
   $(".planet-2").click(function() {
     $('.planet-2').toggleClass('transform-active');
     $('.explore-2').toggleClass('explore-show');
+    $('.explore-text-2').toggleClass('explore-text-show');
     $('.planet-5, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-2").click(function() {
@@ -45,17 +48,19 @@ $(document).ready(function(){
     $('.return-to-orbit-2').toggleClass('return-to-orbit-show')
     $('.planet-2').toggleClass('transform-active');
     $('.explore-2').toggleClass('explore-show');
+    $('.explore-text-2').toggleClass('explore-text-show');
     $('.planet-2').addClass('on-top');
     setTimeout(function() {
       $('.planet-5, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-2').removeClass('on-top');
-    }, 3000);
+    }, 2000);
   })
 
 
   $(".planet-3").click(function() {
     $('.planet-3').toggleClass('transform-active');
     $('.explore-3').toggleClass('explore-show');
+    $('.explore-text-3').toggleClass('explore-text-show');
     $('.planet-2, .planet-5, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-3").click(function() {
@@ -71,17 +76,19 @@ $(document).ready(function(){
     $('.return-to-orbit-3').toggleClass('return-to-orbit-show')
     $('.planet-3').toggleClass('transform-active');
     $('.explore-3').toggleClass('explore-show');
+    $('.explore-text-3').toggleClass('explore-text-show');
     $('.planet-3').addClass('on-top');
     setTimeout(function() {
       $('.planet-2, .planet-5, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-3').removeClass('on-top');
-    }, 3000);
+    }, 2000);
   })
 
 
   $(".planet-4").click(function() {
     $('.planet-4').toggleClass('transform-active');
     $('.explore-4').toggleClass('explore-show');
+    $('.explore-text-4').toggleClass('explore-text-show');
     $('.planet-2, .planet-3, .planet-5, .planet-1').toggleClass('go-away');
   });
   $(".explore-4").click(function() {
@@ -97,17 +104,19 @@ $(document).ready(function(){
     $('.return-to-orbit-4').toggleClass('return-to-orbit-show')
     $('.planet-4').toggleClass('transform-active');
     $('.explore-4').toggleClass('explore-show');
+    $('.explore-text-4').toggleClass('explore-text-show');
     $('.planet-4').addClass('on-top');
     setTimeout(function() {
       $('.planet-2, .planet-3, .planet-5, .planet-1').toggleClass('go-away');
       $('.planet-4').removeClass('on-top');
-    }, 3000);
+    }, 2000);
   })
 
 
   $(".planet-5").click(function() {
     $('.planet-5').toggleClass('transform-active');
     $('.explore-5').toggleClass('explore-show');
+    $('.explore-text-5').toggleClass('explore-text-show');
     $('.planet-2, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
   });
   $(".explore-5").click(function() {
@@ -123,11 +132,12 @@ $(document).ready(function(){
     $('.return-to-orbit-5').toggleClass('return-to-orbit-show')
     $('.planet-5').toggleClass('transform-active');
     $('.explore-5').toggleClass('explore-show');
+    $('.explore-text-5').toggleClass('explore-text-show');
     $('.planet-5').addClass('on-top');
     setTimeout(function() {
       $('.planet-2, .planet-3, .planet-4, .planet-1').toggleClass('go-away');
       $('.planet-5').removeClass('on-top');
-    }, 3000);
+    }, 2000);
   })
 
 });
