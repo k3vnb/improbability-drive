825159e4185a77a65fe22d4b699efd0b37cc7626
Guest User <Guest@Epicodus-5D.local>
2017-12-21 11:05:25 -0800

    fix scripts

--- js/scripts.js
+++ js/scripts.js
@@ -222,15 +222,10 @@ $(document).ready(function() {
 //planet zoom locator
 $(document).ready(function(e) {
     $('.planets > div').click(function(e) {
-
       var xPercent = e.pageX / $(window).width();
       var yPercent = e.pageY / $(window).height();
-      // var xPosition = Math.floor(xPercent*100);
-      // var yPosition = Math.floor(yPercent*100);
       var xPosition = Math.floor(xPercent*100);
       var yPosition = Math.floor(yPercent*100);
-      console.log(xPosition);
-      console.log(yPosition);
       $('#warp').css({perspectiveOrigin: xPosition + "vw " + yPosition + "vh"});
       $('#warp').addClass("center-warp").delay(5000).queue(function(next){
         $('#warp').removeClass("center-warp");
